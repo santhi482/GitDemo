4be8c301a12f80d6f6c78cd5ada37ee7b0e47cc6
Santhi482 <santhipriya.a@abhibus.com>
2019-07-18 17:04:06 +0530

    Committed from user2

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("hi")
         print("12345")
+        print("committed from checkout version")
         // Do any additional setup after loading the view.
     }
 
