b36ae625dfed0e72504823e0b4febfa063379e38
Santhi Piya <santhipriya.a@abhibus.com>
2019-07-18 15:04:50 +0530

    Commit 2

--- GitDemo/ViewController.swift
+++ GitDemo/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hi")
+        print("12345")
         // Do any additional setup after loading the view.
     }
 
